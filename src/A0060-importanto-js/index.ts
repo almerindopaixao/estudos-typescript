32ba3e25bd61c0c15b2951cf8a9121ff04c44724
Almerindo Paixão <almerindopaixao@gmail.com>
2020-10-05 09:19:04 -0300

    Update A0060-impotando-js

---
+++ src/A0060-importanto-js/index.ts
@@ -0,0 +1,5 @@
+import soma from './module';
+
+const result = soma(1, 2);
+
+console.log(result);
---
+++ src/A0060-importanto-js/module.js
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+export default function (x, y) {
+  return x + y;
+}
