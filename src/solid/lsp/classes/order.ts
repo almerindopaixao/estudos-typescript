72ff187cd77020fa83aee7ebd0805e57edf63544
almerindopaixao <almerindo_paixao@live.com>
2020-10-07 18:50:09 -0300

    Update lsp

---
+++ src/solid/lsp/classes/order.ts
@@ -0,0 +1,33 @@
+import { OrderStatus } from './interfaces/cart-item';
+import { ShoppingCart } from './shopping-cart';
+import { Message } from '../services/message';
+import { Persistence } from '../services/persistence';
+
+export class Order {
+  private _orderStatus: OrderStatus = 'open';
+
+  constructor(
+    private readonly cart: ShoppingCart,
+    private readonly message: Message,
+    private readonly persistence: Persistence,
+  ) {}
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      console.log('Seu carrinho está vazio');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+
+    this.message.sendMessage(
+      `Seu pedido com total de R$${this.cart.totalWithDicount()} foi recebido com sucesso`,
+    );
+    this.persistence.saveOrder();
+    this.cart.clear();
+  }
+}
---
+++ src/solid/lsp/main.ts
@@ -0,0 +1,28 @@
+/*
+Liskov substitution principle (Príncipio da substituição de Liskov)
+*/
+
+import { Order } from './classes/order';
+import { ShoppingCart } from './classes/shopping-cart';
+import { Message } from './services/message';
+import { Persistence } from './services/persistence';
+import { Product } from './classes/product';
+import { NoDiscount } from './classes/discounnt';
+
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const message = new Message();
+const persistence = new Persistence();
+const order = new Order(shoppingCart, message, persistence);
+
+shoppingCart.addItem(new Product('Camiseta', 49.91));
+shoppingCart.addItem(new Product('Caderno', 9.9123));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+// shoppingCart.clear();
+
+console.log(order.orderStatus);
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDicount());
+order.checkout();
+console.log(order.orderStatus);
